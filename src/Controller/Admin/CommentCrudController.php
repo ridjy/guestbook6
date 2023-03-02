ff2138e32e7a306c53af67a2e1e19ab5a74ff1bb
Eddy <eddy@qdata-mg.com>
2023-03-02 14:30:54 +0300

    affichage photo sur interface admin

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -11,6 +11,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class CommentCrudController extends AbstractCrudController
@@ -43,8 +44,10 @@ class CommentCrudController extends AbstractCrudController
         yield TextField::new('auteur');
         yield EmailField::new('email');
         yield TextareaField::new('commentaire')->hideOnIndex();
-        yield TextField::new('photoFilename');
-        //yield TextField::new('photoFilename')->onlyOnIndex();
+        yield ImageField::new('photoFilename')
+            ->setBasePath('/uploads/photos')
+            ->setLabel('Photo')
+            ->onlyOnIndex();
 
         $createdAt = DateTimeField::new('dateCreation')->setFormTypeOptions([
             'html5' => true,
