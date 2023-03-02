9344f43d6910857badbea60fba447b3699aa9d6d
Eddy <eddy@qdata-mg.com>
2023-03-02 14:26:08 +0300

    insertion photo en commentaire ok

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,9 +9,18 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ConferenceRepository;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
+use App\Form\CommentFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Comment;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class ConferenceController extends AbstractController
 {
+    public function __construct(private EntityManagerInterface $entityManager) 
+    {
+    }
+
     #[Route('/', name: 'homepage')]
     public function index(ConferenceRepository $conferenceRepository): Response
     {
@@ -21,8 +30,30 @@ class ConferenceController extends AbstractController
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, #[Autowire('%photo_dir%')] string $photoDir): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            if ($photo = $form['photoFilename']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // unable to upload the photo, give up
+                }
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -31,6 +62,7 @@ class ConferenceController extends AbstractController
         'comments' => $paginator,
         'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
         'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+        'comment_form' => $form,
         ]);
     }
 }
