11cef65bbf582c09daccd7146fba2589426a94f2
Eddy <eddy@qdata-mg.com>
2023-03-09 18:22:14 +0300

    test soumission formulaire

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase
 {
-    /*public function testSomething(): void
+    public function testSomething(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'Hello World');
-    }*/
+        //$this->assertSelectorTextContains('h1', 'Hello World');
+    }
 
     public function testConferencePage(): void
     {
@@ -23,11 +23,27 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertCount(2, $crawler->filter('h4'));
 
         $client->clickLink('Afficher');
+        //$client->click($crawler->filter('h4  p a')->link());
 
         $this->assertPageTitleContains('Amsterdam');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
         $this->assertSelectorExists('div:contains("There are 1 comments")');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_form[auteur]' => 'Fabien',
+            'comment_form[commentaire]' => 'Some feedback from an automated functional test',
+            'comment_form[email]' => 'me@automat.ed',
+            'comment_form[photoFilename]' => dirname(__DIR__, 2).'/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("There are 2 comments")');
     }
 
 }
